f5e870b0aa2ff85abc1fc5d45182d2ea2173ee6d
akshaysb100 <akshaybavalekar100@gmail.com>
2020-01-02 20:40:47 +0530

    Added:Code and Test case for Parking vehicle into provided parking lot

--- src/main/java/parkinglotsystem/ParkingLotsSystem.java
+++ src/main/java/parkinglotsystem/ParkingLotsSystem.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class ParkingLotsSystem {
     private int lotCapacity;
-    private List<ParkingLot> parkingLots = new ArrayList<>();
+    private List<ParkingLot> parkingLots;
 
     public ParkingLotsSystem(int lotCapacity) {
         this.lotCapacity= lotCapacity;
+        parkingLots = new ArrayList<>();
     }
 
     public void addLot(ParkingLot parkingLot) {
@@ -19,6 +20,16 @@ public class ParkingLotsSystem {
     public boolean isLotAdded(ParkingLot parkingLot) {
         if (this.parkingLots.contains(parkingLot))
             return true;
+        return false;
+    }
+
+    public boolean parkVehicle(Object vehicle, ParkingLot parkingLot1) throws ParkingLotException {
+       parkingLot1.setCapacity(10);
+       parkingLot1.initializeParkingLot();
+        if (parkingLots.contains(parkingLot1)){
+            parkingLot1.parkVehicle(vehicle);
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/parkinglottest/ParkingLotSystemTest.java
+++ src/test/java/parkinglottest/ParkingLotSystemTest.java
@@ -4,23 +4,39 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import parkinglotsystem.ParkingLot;
+import parkinglotsystem.ParkingLotException;
 import parkinglotsystem.ParkingLotsSystem;
 
 public class ParkingLotSystemTest {
 
     ParkingLotsSystem parkingLotsSystem;
-    ParkingLot parkingLot;
+    ParkingLot parkingLot1;
+    Object vehicle;
     @Before
 
     public void setUp() throws Exception {
         parkingLotsSystem = new ParkingLotsSystem(1);
-        parkingLot = new ParkingLot(10);
+        parkingLot1 = new ParkingLot(10);
+        vehicle = new Object();
     }
 
     @Test
-    public void givenParkingLotSystem_WhenAddLot_ShouldReturnTrue() {
-        parkingLotsSystem.addLot(parkingLot);
-        boolean isLotAdded = parkingLotsSystem.isLotAdded(parkingLot);
+    public void givenParkingLotSystem_WhenAddedLots_ShouldReturnTrue() {
+        ParkingLot parkingLot2 = new ParkingLot(10);
+        parkingLotsSystem.addLot(parkingLot1);
+        parkingLotsSystem.addLot(parkingLot2);
+        boolean isLotAdded = parkingLotsSystem.isLotAdded(parkingLot2);
         Assert.assertTrue(isLotAdded);
+    }
+
+    @Test
+    public void givenParkingLotSystem_WhenVehicleParkedOnLot_ShouldReturnTrue() {
+        parkingLotsSystem.addLot(parkingLot1);
+        try {
+            boolean b = parkingLotsSystem.parkVehicle(vehicle, parkingLot1);
+            Assert.assertTrue(b);
+        } catch (ParkingLotException e) {
+            e.printStackTrace();
+        }
     }
 }
